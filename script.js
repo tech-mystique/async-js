e05762c943e685314f9dc1750cb5848db77d3c18
Nina Antoniuk <nina.antoniuk@socialtechnologies.io>
2023-09-03 17:48:52 +0300

    async js

---
+++ script.js
@@ -0,0 +1,40 @@
+//pokemons list: Bulbasaur Ivysaur Venusaur Charmander Charmeleon Charizard Squirtle Wartortle Blastoise Caterpie Metapod Butterfree Weedle Kakuna Beedrill Pidgey Pidgeotto Pidgeot Rattata
+
+
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const respose = {
+//             name: 'Alisa'
+//         };
+
+//         // resolve(respose);
+//         reject('unknown error');
+//     }, 1000);  
+// });
+
+// // console.log(promise);
+
+// promise.then(res => console.log(res)).catch(err => console.log('error', err))
+
+const domain = 'https://pokeapi.co/api/v2';
+const body = document.body;
+
+const listItemTemplate = ({ name, url}) => {
+    return `<li><p>ability name: ${name}</p><p>ability url: ${url}</p></li>`
+}
+
+const cardTemplate = (imageSrc, imageAlt, pokemonName, pokemonAbalities) => {
+    const list = pokemonAbalities.map((el) => {
+        return listItemTemplate(el.ability)
+    });
+
+    return `<div class="card"> <img src="${imageSrc}" alt="${imageAlt}" /> <p class="name">${pokemonName}</p><ul>${list}</ul></div>`
+}
+
+fetch(`${domain}/pokemon/ditto`)
+    .then(res => res.json())
+    .then(res => {
+        body.insertAdjacentHTML('beforeend', cardTemplate(res.sprites.front_default, res.name, res.name, res.abilities));
+    })
+    .catch(err => console.log('error', err));
+
